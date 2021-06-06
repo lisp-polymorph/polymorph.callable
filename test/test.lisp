d5d172aa38be94f467bdaf9bac378ba9f4836ed8
Alexander Gutev <alex.gutev@gmail.com>
2021-06-06 19:34:36 +0200

    Add unit tests for APPLY function.

--- test/test.lisp
+++ test/test.lisp
@@ -53,6 +53,8 @@
   (is (= 3 (funcall (lambda (x) (/ x 4)) 12))))
 
 (test-optimize funcall-symbol
+  "Test FUNCALL on SYMBOL"
+
   (declare (inline my-car))
 
   (let ((my-add 'my-add))
@@ -63,3 +65,36 @@
       (is (= 15 (funcall 'my-add 4 5 6)))
       (is (= 30 (funcall my-add 10 20)))
       (is (eq 'c (funcall 'my-car '(c d e)))))))
+
+;;;; APPLY
+
+(test-optimize apply-function
+  "Test APPLY on FUNCTION object"
+
+  (let ((f #'my-add))
+    (is (= 12 (apply #'my-add 1 2 3 '(4 2))))
+    (is (= 18 (apply f 1 2 3 4 '(5 3))))))
+
+(test-optimize apply-setf-function
+  "Test APPLY on (SETF ...) function"
+
+  (let ((cell (list 1 2 3)))
+    (is (eq 'a (apply #'(setf my-car) (list 'a cell))))
+    (is (equal '(a 2 3) cell))))
+
+(test-optimize apply-lambda
+  "Test APPLY on LAMBDA function"
+
+  (is (= 110 (apply #'(lambda (a b) (* a b)) 10 '(11))))
+  (is (= 3 (apply #'(lambda (x) (/ x 4)) '(12)))))
+
+(test-optimize apply-symbol
+  "Test APPLY on SYMBOL"
+
+  (let ((my-add 'my-add))
+    (flet ((my-add (x)
+             (error "Gotcha. Lexical MY-ADD function called instead of global function.")))
+      (declare (inline my-add))
+
+      (is (= 15 (apply 'my-add 4 5 '(3 3))))
+      (is (= 30 (apply my-add 10 15 '(2 3)))))))
